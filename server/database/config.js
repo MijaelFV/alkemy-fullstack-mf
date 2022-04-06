dfe31f02b49b0eeeee9704fba5a2c0586fdfaded
MijaelFV <fvmijael@gmail.com>
2022-04-06 14:36:04 -0300

    change database from postgres to mariadb

--- server/database/config.js
+++ server/database/config.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_NAME = 'alkemydb' , process.env.DB_USER = 'alkemyuser', process.env.DB_PASSWORD = 'alkemy', {
+const db = new Sequelize(process.env.DB_NAME = 'challengedb' , process.env.DB_USER = 'alkemyuser', process.env.DB_PASSWORD = 'alkemy', {
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mariadb',
     logging: false,
 });
 
