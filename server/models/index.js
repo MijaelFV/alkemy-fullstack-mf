f090fe390aa0046218e4312e37498de7e750b4f6
MijaelFV <fvmijael@gmail.com>
2022-04-07 21:25:38 -0300

    add to the User-Entry association the option CASCADE and not allow NULL

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,13 @@
 const User = require('./user_model')
-const Entry = require('./entry_model')
+const Entry = require('./entry_model');
 
 // Create asociations
-User.hasMany(Entry)
-Entry.belongsTo(User);
+User.hasMany(Entry, {
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+  foreignKey: {name: 'userId', allowNull: false}
+})
+Entry.belongsTo(User, {foreignKey: {name: 'userId', allowNull: false}});
 //
 
 // Force database sync
