dad0c0f3240daf4f555ba5e311f04094c192ca21
MijaelFV <fvmijael@gmail.com>
2022-04-12 16:14:28 -0300

    add menu & logout button

---
+++ components/ui/MainMenu.js
@@ -0,0 +1,48 @@
+import { Category, Logout } from "@mui/icons-material";
+import { ListItemIcon, ListItemText, Menu, MenuItem } from "@mui/material";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/auth/AuthContext";
+import { UiContext } from "../../context/ui/UiContext";
+
+export const MainMenu = () => {
+
+  const {isMenuOpen, toggleMenu, toggleDrawer, setDrawerForm} = useContext(UiContext)
+  const {logout} = useContext(AuthContext)
+  const open = Boolean(isMenuOpen);
+
+  const handleCloseMenu = () => {
+    toggleMenu(null)
+  };
+
+  const handleLogout = () => {
+    handleCloseMenu()
+    logout()
+  }
+
+  // const handleCategoryMenu = () => {
+  //   handleCloseMenu()
+  //   setDrawerForm('category')
+  //   toggleDrawer()
+  // }
+
+  return (
+    <Menu
+      anchorEl={isMenuOpen}
+      open={open}
+      onClose={handleCloseMenu}
+    >
+      {/* <MenuItem onClick={handleCategoryMenu}>
+        <ListItemIcon>
+          <Category />
+        </ListItemIcon>
+        <ListItemText>Manage categories</ListItemText>
+      </MenuItem> */}
+      <MenuItem onClick={handleLogout}>
+        <ListItemIcon>
+          <Logout />
+        </ListItemIcon>
+        <ListItemText>Log out</ListItemText>
+      </MenuItem>
+    </Menu>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,42 @@
 import AddIcon from '@mui/icons-material/Add';
-import { Card, CardContent, Fab, Grid, Typography } from "@mui/material"
+import { Box, IconButton, Card, CardContent, Fab, Grid, Typography, MenuItem, ListItemIcon, MenuList, ListItemText } from "@mui/material"
 import { format } from "../utils/currency"
 import { EntryList } from "../components/balance/EntryList"
 import { MainLayout } from "../components/layouts/MainLayout";
+// import { Bottombar } from "../components/ui/Bottombar";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useContext } from "react";
+import { MainMenu } from "../components/ui/MainMenu";
+import { UiContext } from "../context/ui/UiContext";
 
-export default function Home({entries}) {
+export default function HomePage({entries}) {
+
+  const {toggleDrawer, setDrawerForm, toggleMenu} = useContext(UiContext)
+
+  // const handleOpenCreateEntry = () => {
+  //   setDrawerForm('create')
+  //   toggleDrawer()
+  // }
+
+  const handleOpenMenu = (event) => {
+    toggleMenu(event.currentTarget)
+  }
+
   return (
     <MainLayout title='Balance'>
       <Grid container flex justifyContent={'center'}>
         <Grid item xs={11} sm={10} lg={6}>
-          <Typography variant="h6" fontWeight={'medium'} mt={2} mb={2}>Hi, Mijael!</Typography>
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Typography variant="h6" fontWeight={'medium'} mt={2} mb={2}>Hi, Mijael!</Typography>
+            <IconButton
+              variant="contained"
+              color="primary"
+              onClick={handleOpenMenu}
+            >
+              <MenuIcon sx={{width: 30, height: 30}} />
+            </IconButton>
+            <MainMenu />
+          </Box>
           <Card sx={{bgcolor:"#fd751a", color:"white"}}>
             <CardContent sx={{display: "flex", flexDirection: "column", justifyContent:"center", alignItems:"center"}}>
               <Typography variant="h6">Balance</Typography>
@@ -17,9 +44,10 @@ export default function Home({entries}) {
             </CardContent>
           </Card>
           <EntryList />
-          <Fab sx={{position: 'fixed', bottom: 16, right: 16}} color="secondary">
+          {/* <Fab onClick={handleOpenCreateEntry} sx={{position: 'fixed', bottom: 16, right: 16}} color="secondary">
             <AddIcon />
-          </Fab>
+          </Fab> */}
+          {/* <Bottombar /> */}
         </Grid>
       </Grid>
     </MainLayout>
