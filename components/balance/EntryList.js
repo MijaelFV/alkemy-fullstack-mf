53d4cf9b2a3a49ca1bdad685589c2b302aefb2dd
MijaelFV <fvmijael@gmail.com>
2022-04-13 01:46:19 -0300

    add  message when entryList is empty

--- components/balance/EntryList.js
+++ components/balance/EntryList.js
@@ -1,8 +1,9 @@
 import { Refresh } from "@mui/icons-material"
-import { Box, Grid, IconButton, Typography } from "@mui/material"
+import { Box, Grid, IconButton, Typography, List, ListItem } from "@mui/material"
 import { useContext } from "react"
 import { EntryContext } from "../../context/entry/EntryContext"
 import { EntryCard } from "./EntryCard"
+import AddCircle from '@mui/icons-material/AddCircle';
 
 export const EntryList = ({entries}) => {
 
@@ -30,9 +31,23 @@ export const EntryList = ({entries}) => {
           </IconButton>
         </Grid>
       {
-          entries.sort((a,b) => sorting(a, b)).map(entry => (
-              <EntryCard key={entry.id} entry={entry} />
-          ))
+          entries.length > 0
+          ? (
+            entries.sort((a,b) => sorting(a, b)).map(entry => (
+                <EntryCard key={entry.id} entry={entry} />
+            ))
+          )
+          : (
+            <Grid mt={2} item display="flex" flexDirection="column" alignItems="center">
+              <Typography mb={1} variant="body1" color="gray" maxWidth={300} textAlign="center">
+                There are no entries registered.
+              </Typography>
+              <Typography mb={2} variant="body1" color="gray" maxWidth={300} textAlign="center">
+                You can start adding new ones with the floating button.
+              </Typography>
+              <AddCircle sx={{mb: 1,height: 35, width: 35, color: "#afafaf"}} />
+            </Grid>
+          )
       }
       </Grid>
     </Box>
