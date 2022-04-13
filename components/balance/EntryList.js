b12332a9756cbd18d3a43f5edafdb128343e4008
MijaelFV <fvmijael@gmail.com>
2022-04-12 23:36:31 -0300

    add sorting function to entryList

--- components/balance/EntryList.js
+++ components/balance/EntryList.js
@@ -1,15 +1,37 @@
-import { Box, Grid, Typography } from "@mui/material"
+import { Refresh } from "@mui/icons-material"
+import { Box, Grid, IconButton, Typography } from "@mui/material"
+import { useContext } from "react"
+import { EntryContext } from "../../context/entry/EntryContext"
 import { EntryCard } from "./EntryCard"
 
 export const EntryList = ({entries}) => {
 
+  const {refreshEntries} = useContext(EntryContext)
+
+  const sorting = (a, b) => {
+    if (a.date > b.date) {
+      return -1;
+    }
+
+    if (a.date < b.date) {
+      return 1;
+    }
+
+    return 0;
+  }
+
   return (
     <Box mt={2} mb={10}>
-      <Typography variant="h6" fontWeight={'medium'} mb={1}>Entries</Typography>
       <Grid container spacing={1} flex flexDirection="column" >
+        <Grid item display="flex" justifyContent="space-between" alignItems="end">
+          <Typography variant="h6" fontWeight={'medium'} mb={1}>Last entries</Typography>
+          <IconButton color="primary" onClick={refreshEntries}>
+            <Refresh sx={{height: 30, width: 30}} />
+          </IconButton>
+        </Grid>
       {
-          entries.map(entry => (
-              <EntryCard key={entry.concept} entry={entry} />
+          entries.sort((a,b) => sorting(a, b)).map(entry => (
+              <EntryCard key={entry.id} entry={entry} />
           ))
       }
       </Grid>
